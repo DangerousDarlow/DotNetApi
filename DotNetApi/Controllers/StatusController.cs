0fa703f455c6a72d9851f4af29922f7c1f888f7c
Nick Darlow <dangerous.darlow@gmail.com>
2024-06-05 09:05:26 +0100

    Don't catch Postgres exceptions in the database status endpoint

--- DotNetApi/Controllers/StatusController.cs
+++ DotNetApi/Controllers/StatusController.cs
@@ -1,7 +1,5 @@
-﻿using System.Net.Sockets;
-using DotNetApi.Model.Status;
+﻿using DotNetApi.Model.Status;
 using Microsoft.AspNetCore.Mvc;
-using Npgsql;
 
 namespace DotNetApi.Controllers;
 
@@ -15,21 +13,10 @@ public class StatusController(StatusRepository repository) : ControllerBase
     [HttpGet("database")]
     public async Task<DatabaseStatusResponse?> GetDatabaseStatus()
     {
-        try
-        {
-            Response.StatusCode = 200;
-            var applicationTime = DateTime.UtcNow;
-            var databaseTime = await repository.GetNow();
-            var difference = databaseTime - applicationTime;
-            return new DatabaseStatusResponse(applicationTime, databaseTime, difference);
-        }
-        catch (NpgsqlException e)
-        {
-            if (e.InnerException is not SocketException)
-                throw;
-
-            Response.StatusCode = 408;
-            return null;
-        }
+        Response.StatusCode = 200;
+        var applicationTime = DateTime.UtcNow;
+        var databaseTime = await repository.GetNow();
+        var difference = databaseTime - applicationTime;
+        return new DatabaseStatusResponse(applicationTime, databaseTime, difference);
     }
 }
