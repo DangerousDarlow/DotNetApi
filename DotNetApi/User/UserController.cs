4c128d90449156f72f844eabfc4ab55e2027ae2e
Nick Darlow <dangerous.darlow@gmail.com>
2024-06-17 22:39:21 +0100

    Handle add user db failure due to conflict

--- DotNetApi/User/UserController.cs
+++ DotNetApi/User/UserController.cs
@@ -4,14 +4,25 @@ namespace DotNetApi.User;
 
 [ApiController]
 [Route("[controller]")]
-public class UserController(UserRepository repository)
+public class UserController(UserRepository repository) : ControllerBase
 {
     [HttpGet]
-    public Task<IEnumerable<User>> GetUsers() => repository.GetUsers();
+    public async Task<IEnumerable<User>> GetUsers() => await repository.GetUsers();
 
     [HttpPost]
-    public Task AddUser(User user) => repository.AddUser(user);
+    public async Task<IActionResult> AddUser(User user)
+    {
+        try
+        {
+            await repository.AddUser(user);
+            return Ok();
+        }
+        catch (ConflictException)
+        {
+            return Conflict(new { error = "UserAlreadyExists", message = "The user you are trying to create already exists" });
+        }
+    }
 
     [HttpDelete("{id:guid}")]
-    public Task DeleteUser(Guid id) => repository.DeleteUser(id.ToUserId());
+    public async Task DeleteUser(Guid id) => await repository.DeleteUser(id.ToUserId());
 }
--- DotNetApi/User/UserRepository.cs
+++ DotNetApi/User/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using Dapper;
+using Npgsql;
 
 namespace DotNetApi.User;
 
@@ -7,7 +8,22 @@ public class UserRepository(IDbConnection dbConnection)
 {
     public Task<IEnumerable<User>> GetUsers() => dbConnection.QueryAsync<User>("SELECT id, name FROM users");
 
-    public Task<int> AddUser(User user) => dbConnection.ExecuteAsync("INSERT INTO users (id, name) VALUES (@id, @name)", user);
+    public async Task<int> AddUser(User user)
+    {
+        try
+        {
+            return await dbConnection.ExecuteAsync("INSERT INTO users (id, name) VALUES (@id, @name)", user);
+        }
+        catch (NpgsqlException e)
+        {
+            if (e.SqlState == PostgresErrorCodes.UniqueViolation)
+                throw new ConflictException("User already exists", e);
+
+            throw;
+        }
+    }
 
     public Task<int> DeleteUser(UserId id) => dbConnection.ExecuteAsync("DELETE FROM users WHERE id = @value", id);
 }
+
+public class ConflictException(string message, Exception e) : Exception(message, e);
