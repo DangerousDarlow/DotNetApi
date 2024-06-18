49feeb80c6fd2a5a907989bb73e31f45d6076dc0
Nick Darlow <dangerous.darlow@gmail.com>
2024-06-18 08:46:00 +0100

    Add additional test for adding same user twice

--- DotNetApi.IntegrationTests/UserTests.cs
+++ DotNetApi.IntegrationTests/UserTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 
@@ -6,7 +7,7 @@ namespace DotNetApi.IntegrationTests;
 public class UserTests : IntegrationTest
 {
     [Test]
-    public async Task AddGetDeleteUser()
+    public async Task Add_then_get_then_delete_user()
     {
         var userId = Guid.NewGuid();
 
@@ -32,5 +33,20 @@ public class UserTests : IntegrationTest
         getResponseContent = await getResponse.Content.ReadAsStringAsync();
         users = JsonSerializer.Deserialize<IEnumerable<User.User>>(getResponseContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         Assert.That(users?.Any(x => x.Id == userId), Is.False);
+    }
+
+    [Test]
+    public async Task Add_same_user_twice_returns_conflict()
+    {
+        var userId = Guid.NewGuid();
+
+        var addResponse = await Client.PostAsJsonAsync("/user", new { Id = userId, Name = "Test" });
+        Assert.That(addResponse.IsSuccessStatusCode, Is.True);
+
+        addResponse = await Client.PostAsJsonAsync("/user", new { Id = userId, Name = "Test" });
+        Assert.That(addResponse.StatusCode, Is.EqualTo(HttpStatusCode.Conflict));
+
+        var deleteResponse = await Client.DeleteAsync($"/user/{userId}");
+        Assert.That(deleteResponse.IsSuccessStatusCode, Is.True);
     }
 }
